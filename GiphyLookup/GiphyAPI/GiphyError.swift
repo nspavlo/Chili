4b3356e7c1cb9bcbefad0dd18c7b9c5c5be2d8c8
Jans Pavlovs <jans.pavlovs@gmail.com>
2022-11-20 18:39:22 +0200

    Remove error codes from user facing errors

--- GiphyLookup/GiphyAPI/GiphyError.swift
+++ GiphyLookup/GiphyAPI/GiphyError.swift
@@ -5,6 +5,10 @@
 //  Created by Jans Pavlovs on 20/11/2022.
 //
 
+import Foundation
+
+// MARK: Initialization
+
 public enum GiphyError: Error {
     public enum MapperError: Error {
         case response(statusCode: Int)
@@ -13,4 +17,17 @@ public enum GiphyError: Error {
 
     case parsing(underlayingError: MapperError)
     case network(underlayingError: Error)
+}
+
+// MARK: LocalizedError
+
+extension GiphyError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .parsing(underlayingError: let underlayingError):
+            return underlayingError.localizedDescription
+        case .network(underlayingError: let underlayingError):
+            return underlayingError.localizedDescription
+        }
+    }
 }
