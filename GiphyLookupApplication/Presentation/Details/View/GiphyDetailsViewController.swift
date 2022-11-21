999d20d20a2ea88a7b7385ff9fe5c647fb70f74c
Jans Pavlovs <jans.pavlovs@gmail.com>
2022-11-21 10:27:15 +0200

    Add empty `GiphyDetailsViewController`

---
+++ GiphyLookupApplication/Presentation/Details/View/GiphyDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GiphyDetailsViewController.swift
+//  GiphyLookupApplication
+//
+//  Created by Jans Pavlovs on 21/11/2022.
+//
+
+import UIKit
+
+// MARK: -
+
+// MARK: Initialization
+
+final class GiphyDetailsViewController: UIViewController {}
